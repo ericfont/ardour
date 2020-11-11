9ec649f330eb7acaf0c0db74413ad9dfa4da2adf
Paul Davis <paul@linuxaudiosystems.com>
2020-11-11 09:31:52 -0700

    random commit to facilitate trivial move of work from laptop back to main system

--- libs/ardour/ardour/session.h
+++ libs/ardour/ardour/session.h
@@ -115,6 +115,10 @@ namespace Evoral {
 class Curve;
 }
 
+namespace Temporal {
+class TempoMap;
+}
+
 namespace ARDOUR {
 
 class Amp;
@@ -167,7 +171,6 @@ class SessionPlaylists;
 class SoloMuteRelease;
 class Source;
 class Speakers;
-class TempoMap;
 class TransportMaster;
 struct TransportFSM;
 class Track;
@@ -774,7 +777,7 @@ public:
 
 	void sync_time_vars();
 
-	void bbt_time (samplepos_t when, Temporal::BBT_Time&);
+	void bbt_time (Temporal::timepos_t const & when, Temporal::BBT_Time&);
 	void timecode_to_sample(Timecode::Time& timecode, samplepos_t& sample, bool use_offset, bool use_subframes) const;
 	void sample_to_timecode(samplepos_t sample, Timecode::Time& timecode, bool use_offset, bool use_subframes) const;
 	void timecode_time (Timecode::Time &);
@@ -821,8 +824,8 @@ public:
 	bool loop_is_possible () const;
 	PBD::Signal0<void> PunchLoopConstraintChange;
 
-	TempoMap&       tempo_map()       { return *_tempo_map; }
-	const TempoMap& tempo_map() const { return *_tempo_map; }
+	Temporal::TempoMap&       tempo_map()       { return *_tempo_map; }
+	const Temporal::TempoMap& tempo_map() const { return *_tempo_map; }
 	void maybe_update_tempo_from_midiclock_tempo (float bpm);
 
 	unsigned int    get_xrun_count () const {return _xrun_count; }
@@ -1829,8 +1832,8 @@ private:
 	int  send_full_time_code (samplepos_t, pframes_t nframes);
 	void send_song_position_pointer (samplepos_t);
 
-	TempoMap    *_tempo_map;
-	void          tempo_map_changed (const PBD::PropertyChange&);
+	Temporal::TempoMap    *_tempo_map;
+	void          tempo_map_changed ();
 
 	/* edit/mix groups */
 
